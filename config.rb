ab3c0efea1fdaffdbda6d0a3f0effcc2c92be752
Peter Booker <mail@peterbooker.com>
2013-04-22 14:40:45 +0100

    removed file, various changes

--- config.rb
+++ config.rb
@@ -6,7 +6,7 @@ require 'zurb-foundation'
 http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
-images_dir = "images"
+images_dir = "imgs"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
