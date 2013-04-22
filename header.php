deb33915798195b270ffc3baea4f9d7f778499df
Peter Booker <mail@peterbooker.com>
2013-04-22 15:20:57 +0100

    replaced hgroup element with a div tag, hgroup has been removed from the html5 standard

--- header.php
+++ header.php
@@ -6,36 +6,42 @@
  *
  * @package Kebo
  */
-?><!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
-<title><?php wp_title( '|', true, 'right' ); ?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<!--[if lt IE 9]>
-<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
-<![endif]-->
+?>
+<!DOCTYPE html>
+<!--[if IE 8]> <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
+    <html <?php language_attributes(); ?>>
+        <head>
+            <meta charset="<?php bloginfo('charset'); ?>" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            
+            <title><?php wp_title('|', true, 'right'); ?></title>
+            
+            <link rel="profile" href="http://gmpg.org/xfn/11" />
+            <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
-<?php wp_head(); ?>
-</head>
+            <?php wp_head(); ?>
+        </head>
 
-<body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
-	<?php do_action( 'before' ); ?>
-	<header id="masthead" class="site-header" role="banner">
-		<hgroup>
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</hgroup>
+        <body <?php body_class(); ?>>
+            <div id="page" class="hfeed site row">
+                <?php do_action('before'); ?>
+                <header id="masthead" class="site-header" role="banner">
+                    <div class="hgroup">
+                        <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
+                        <h2 class="site-description"><?php bloginfo('description'); ?></h2>
+                    </div>
 
-		<nav id="site-navigation" class="navigation-main" role="navigation">
-			<h1 class="menu-toggle"><?php _e( 'Menu', 'kebo' ); ?></h1>
-			<div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'kebo' ); ?>"><?php _e( 'Skip to content', 'kebo' ); ?></a></div>
+                    <nav id="site-navigation" class="navigation-main" role="navigation">
+                        <h1 class="menu-toggle"><?php _e('Menu', 'kebo'); ?></h1>
+                        <div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e('Skip to content', 'kebo'); ?>"><?php _e('Skip to content', 'kebo'); ?></a></div>
+                        <?php
+                            $menu = array(
+                                'theme_location' => 'primary',
+                            );
+                        ?>
+                        <?php wp_nav_menu( $menu ); ?>
+                    </nav><!-- #site-navigation -->
+                </header><!-- #masthead -->
 
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
-
-	<div id="main" class="site-main">
+                <div id="main" class="site-main">
