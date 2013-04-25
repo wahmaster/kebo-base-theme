712608aaa534ccda1e25369258d73dbf7b37bced
Peter Booker <mail@peterbooker.com>
2013-04-25 18:26:44 +0100

    hide page search bar for small devices, as the sidebar search input will be in the same position

--- no-results.php
+++ no-results.php
@@ -22,12 +22,12 @@
         <?php elseif (is_search()) : ?>
 
             <p><?php _e('Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'kebo'); ?></p>
-            <?php get_search_form(); ?>
+            <div class="hide-for-small"><?php get_search_form(); ?></div>
 
         <?php else : ?>
 
             <p><?php _e('It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'kebo'); ?></p>
-            <?php get_search_form(); ?>
+            <div class="hide-for-small"><?php get_search_form(); ?></div>
 
         <?php endif; ?>
     </div><!-- .entry-content -->
