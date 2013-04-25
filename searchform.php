cfc4302c1a15145341b6895ef11e2e52e544df75
Peter Booker <mail@peterbooker.com>
2013-04-25 18:24:34 +0100

    trim display of search term to filter empty search

--- searchform.php
+++ searchform.php
@@ -1,14 +1,13 @@
 <?php
 /**
- * The template for displaying search forms in Kebo
+ * The template for displaying search forms
  *
- * @package Kebo
  */
 ?>
 <form method="get" id="searchform" class="searchform" action="<?php echo esc_url(home_url('/')); ?>" role="search">
     
     <label for="s" class="screen-reader-text"><?php _ex('Search', 'assistive text', 'kebo'); ?></label>
-    <input type="search" class="field" name="s" value="<?php echo esc_attr(get_search_query()); ?>" id="s" placeholder="<?php echo esc_attr_x('Search &hellip;', 'placeholder', 'kebo'); ?>" />
+    <input type="search" class="field" name="s" value="<?php echo trim(esc_attr(get_search_query())); ?>" id="s" placeholder="<?php echo esc_attr_x('Search &hellip;', 'placeholder', 'kebo'); ?>" />
     <input type="submit" class="submit" id="searchsubmit" value="<?php echo esc_attr_x('Search', 'submit button', 'kebo'); ?>" />
     
 </form>
