747bc6fa702161c8a7d660d128f08ab21b37eafd
Peter Booker <mail@peterbooker.com>
2013-04-25 19:14:36 +0100

    added paging support

--- author.php
+++ author.php
@@ -47,7 +47,7 @@ get_header();
                 <?php get_template_part('content', get_post_format()); ?>
             <?php endwhile; ?>
 
-            <?php twentythirteen_paging_nav(); ?>
+            <?php kebo_content_nav(); ?>
 
         <?php else : ?>
             <?php get_template_part('content', 'none'); ?>
