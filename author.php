0084de18a05396607c4e7513ac31e4a9e790776a
Peter Booker <mail@peterbooker.com>
2013-04-25 20:01:06 +0100

    cleaned formatting

--- author.php
+++ author.php
@@ -39,18 +39,30 @@ get_header();
             ?>
 
             <?php if (get_the_author_meta('description')) : ?>
+            
                 <?php get_template_part('author-bio'); ?>
+            
             <?php endif; ?>
 
-            <?php /* The loop */ ?>
+            <?php /* Start the Loop */ ?>
             <?php while (have_posts()) : the_post(); ?>
-                <?php get_template_part('content', get_post_format()); ?>
+            
+                <?php
+                /* Include the Post-Format-specific template for the content.
+                 * If you want to overload this in a child theme then include a file
+                 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                 */
+                get_template_part('content', get_post_format());
+                ?>
+            
             <?php endwhile; ?>
 
             <?php kebo_content_nav('nav-below'); ?>
 
         <?php else : ?>
+            
             <?php get_template_part('no-results'); ?>
+            
         <?php endif; ?>
 
     </div><!-- #content .site-content -->
