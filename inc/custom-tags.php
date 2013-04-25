453a05f74c53a978b5a39cebf9d091d825768617
Peter Booker <mail@peterbooker.com>
2013-04-26 00:34:31 +0100

    changed example code to standard menu as default will be foundation top-bar

--- inc/custom-tags.php
+++ inc/custom-tags.php
@@ -7,28 +7,28 @@
 // Custom Menu Walker for Foundation v4 Top Bar Compatibility
 class Kebo_Walker extends Walker_Nav_Menu {
 
-function display_element($element, &$children_elements, $max_depth, $depth = 0, $args, &$output) {
-$element->has_children = !empty($children_elements[$element->ID]);
-$element->classes[] = ($element->current || $element->current_item_ancestor) ? 'active' : '';
-$element->classes[] = ($element->has_children) ? 'has-dropdown' : '';
-parent::display_element($element, $children_elements, $max_depth, $depth, $args, $output);
-}
+    function display_element($element, &$children_elements, $max_depth, $depth = 0, $args, &$output) {
+        $element->has_children = !empty($children_elements[$element->ID]);
+        $element->classes[] = ($element->current || $element->current_item_ancestor) ? 'active' : '';
+        $element->classes[] = ($element->has_children) ? 'has-dropdown' : '';
+        parent::display_element($element, $children_elements, $max_depth, $depth, $args, $output);
+    }
 
-function start_el(&$output, $item, $depth, $args) {
-$item_html = '';
-parent::start_el($item_html, $item, $depth, $args);
-$output .= ($depth == 0) ? '<li class="divider"></li>' : '';
-$classes = empty($item->classes) ? array() : (array) $item->classes;
-if (in_array('section', $classes)) {
-$output .= '<li class="divider"></li>';
-$item_html = preg_replace('/<a[^>]*>(.*)<\/a>/iU', '<label>$1</label>', $item_html);
-}
-$output .= $item_html;
-}
+    function start_el(&$output, $item, $depth, $args) {
+        $item_html = '';
+        parent::start_el($item_html, $item, $depth, $args);
+        $output .= ($depth == 0) ? '<li class="divider"></li>' : '';
+        $classes = empty($item->classes) ? array() : (array) $item->classes;
+            if (in_array('section', $classes)) {
+                $output .= '<li class="divider"></li>';
+                $item_html = preg_replace('/<a[^>]*>(.*)<\/a>/iU', '<label>$1</label>', $item_html);
+            }
+        $output .= $item_html;
+    }
 
-function start_lvl(&$output, $depth = 0, $args = array()) {
-$output .= "\n<ul class=\"dropdown\">\n";
-}
+    function start_lvl(&$output, $depth = 0, $args = array()) {
+        $output .= "\n<ul class=\"dropdown\">\n";
+    }
 
 }
 
