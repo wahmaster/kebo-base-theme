fd10923f451c24f6464f38ff8c4af07b27eb8dac
Peter Booker <mail@peterbooker.com>
2013-04-25 14:23:58 +0100

    linked to relevant sidebar widget

--- sidebar-forum.php
+++ sidebar-forum.php
@@ -8,7 +8,7 @@
 ?>
 <div id="secondary" class="widget-area small-12 large-3 columns" role="complementary">
     <?php do_action('before_sidebar'); ?>
-    <?php if (!dynamic_sidebar('sidebar-2')) : ?>
+    <?php if (!dynamic_sidebar('sidebar-6')) : ?>
 
     <?php endif; // end sidebar widget area ?>
 </div><!-- #secondary .widget-area .small-12 .large-3 .columns -->
