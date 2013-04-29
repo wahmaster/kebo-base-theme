ccaff18bb72417174cf4372f7ba21d9ee072e079
Peter Booker <mail@peterbooker.com>
2013-04-29 14:40:24 +0100

    improved formatting and comments

--- inc/branding.php
+++ inc/branding.php
@@ -1,12 +1,7 @@
 <?php
-
 /**
- * Custom Branded Admin
- *
- * @package kebo_base
- * @since kebo_base 1.0
+ * Custom Branded Control Panel
  */
-/* * ****** TO BE REMOVED ******** */
 
 if (!function_exists('kebo_login_scripts')):
 
@@ -16,10 +11,12 @@ if (!function_exists('kebo_login_scripts')):
     function kebo_login_scripts() {
 
         // Queues the custom login CSS file.
-        wp_enqueue_style('style-login', get_template_directory_uri() . '/css/login.css');
+        wp_enqueue_style('login-style', get_template_directory_uri() . '/css/login.css');
+        
     }
 
     add_action('login_head', 'kebo_login_scripts');
+    
 endif; // kebo_login_scripts
 
 if (!function_exists('kebo_admin_scripts')):
@@ -30,12 +27,15 @@ if (!function_exists('kebo_admin_scripts')):
     function kebo_admin_scripts() {
 
         if (is_admin()) {
+            
             // Queues the custom admin CSS file.
             wp_enqueue_style('style-admin', get_template_directory_uri() . '/css/admin.css');
+            
         }
     }
 
     add_action('admin_enqueue_scripts', 'kebo_admin_scripts');
+    
 endif; // kebo_admin_scripts
 
 if (!function_exists('kebo_login_logo_url')):
@@ -49,4 +49,5 @@ if (!function_exists('kebo_login_logo_url')):
     }
 
     add_filter('login_headerurl', 'kebo_login_logo_url');
+    
 endif; // kebo_login_logo_url
