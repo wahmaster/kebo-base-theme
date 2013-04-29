807e43ae9ff5ae33c1dec203bc8356dc7abc5282
Peter Booker <mail@peterbooker.com>
2013-04-29 11:53:14 +0100

    comment html layout/formatting

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -78,29 +78,23 @@ if (!function_exists('kebo_comment')) :
                 <li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
                     <article id="comment-<?php comment_ID(); ?>" class="comment">
                         
-                        <div class="comment-author vcard">
-                            <?php echo get_avatar($comment, '80'); ?>
-                        <?php printf(__('%s <span class="says">says:</span>', 'kebo'), sprintf('<cite class="fn">%s</cite>', get_comment_author_link())); ?>
+                        <div class="comment-meta vcard">
+                            <?php echo get_avatar($comment, '50'); ?>
+                        <?php printf(__('%s', 'kebo'), sprintf('<cite class="fn comment-author">%s</cite>', get_comment_author_link())); ?>
+                            <time datetime="<?php comment_time('c'); ?>">
+                                <?php echo kebo_time_ago( get_comment_date('G') ); ?>
+                            </time>
+                            <a class="comment-id" href="<?php echo esc_url(get_comment_link($comment->comment_ID)); ?>">#<?php echo $comment->comment_ID; ?></a>
                         </div><!-- .comment-author .vcard -->
                         <?php if ($comment->comment_approved == '0') : ?>
                             <em><?php _e('Your comment is awaiting moderation.', 'kebo'); ?></em>
                             <br />
                         <?php endif; ?>
-                                
-                        <footer>
-                            <div class="comment-meta commentmetadata">
-                                <a href="<?php echo esc_url(get_comment_link($comment->comment_ID)); ?>">
-                                    <time datetime="<?php comment_time('c'); ?>">
-                                        <?php echo kebo_time_ago( get_comment_date('G') ); ?>
-                                        <?php //printf(_x('%1$s at %2$s', '1: date, 2: time', 'kebo'), get_comment_date(), get_comment_time()); ?>
-                                    </time>
-                                </a>
-                                <?php edit_comment_link(__('Edit', 'kebo'), '<span class="edit-link">', '<span>'); ?>
-                            </div><!-- .comment-meta .commentmetadata -->
-                        </footer>
-
-                        <div class="comment-content"><?php comment_text(); ?></div>
-
+                            
+                        <div class="comment-content">
+                            <?php comment_text(); ?>
+                        </div>
+                        <?php edit_comment_link(__('Edit', 'kebo'), '<span class="edit-link">', '<span>'); ?>
                         <div class="reply">
                             <?php comment_reply_link(array_merge($args, array('depth' => $depth, 'max_depth' => $args['max_depth']))); ?>
                         </div><!-- .reply -->
