050f91500b5dcde35d4a0b2627e85613d82a1981
Peter Booker <mail@peterbooker.com>
2013-04-29 13:45:47 +0100

    re-structured footer html

--- footer.php
+++ footer.php
@@ -11,15 +11,21 @@
 	</div><!-- #main .row -->
 
 	<footer id="colophon" class="site-footer row" role="contentinfo">
-		<div class="site-info small-12 large-12 columns">
-                    
-                    <?php if (!is_404()) { get_sidebar('footer'); } ?>
+            
+            <div class="footer-content small-12 large-12 columns">
+                
+                <?php if (!is_404()) { get_sidebar('footer'); } ?>
+                
+            </div>
+            
+            <div class="site-info small-12 large-12 columns">
                     
                     <?php do_action( 'kebo_credits' ); ?>
                     Powered by <a href="http://kebopowered.com/" title="<?php esc_attr_e( 'Kebo - Empowering People', 'kebo' ); ?>" rel="generator"><?php echo __( 'Kebo', 'kebo' ); ?></a>
 		
-                </div><!-- .site-info .small-12 .large-12 -->
-	</footer><!-- #colophon -->
+            </div><!-- .site-info .small-12 .large-12 -->
+            
+	</footer><!-- #colophon .row -->
         
 </div><!-- #page -->
 
