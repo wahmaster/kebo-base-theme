f01caf40f798799df3764e64bcf764ea5cd434a9
lukeketley <mail@lukeketley.com>
2013-05-06 11:12:43 +0100

    Initial

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /**
  * The main template file.
  *
- * Hey This is the most generic template file in a WordPress theme
+ * This is the most generic template file in a WordPress theme
  * and one of the two required files for a theme (the other being style.css).
  * It is used to display a page when nothing more specific matches a query.
  * E.g., it puts together the home page when no home.php file exists.
